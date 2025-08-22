6a438be8ba18227ab48ff0fc88d286879afb1905
Andrea Atzori <37103069+atzoriandrea@users.noreply.github.com>
2025-08-22 13:30:34 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 export OMP_NUM_THREADS=16
 
-CUDA_VISIBLE_DEVICES=0,1 torchrun --nproc_per_node=2 train_v2_guray.py configs/casia_vit_s_qs.py --mode token
+CUDA_VISIBLE_DEVICES=0,1,2,3 torchrun --nproc_per_node=4 train_v2.py configs/ms1mv2_vit_s_qs.py --mode token
 
